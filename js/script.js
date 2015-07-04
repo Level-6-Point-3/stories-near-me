a721383e2314e731faaa26beb14687e903adb1b2
Kevin Leo <kevin.kevinleo@gmail.com>
2015-07-04 19:12:44 +1000

    pics array stores pic objects

--- js/script.js
+++ js/script.js
@@ -6,20 +6,21 @@ d3.json('bushfires.json', function(stories) {
 function stories_to_pics(stories) {
 
 	var pics = [];
-
+	
 	for (var date in stories) {
 
 		stories[date].forEach(function(story) {
-			var url = story["Primary image"];
+			var img = story["Primary image"];
 
-			if (url === "") {
+			if (img === "") {
 				/// got to xml and get random picture from there
 				// d3.xml("http://www.abc.net.au/local/photos/2013/08/28/3836057-mediarss.xml", function(data) {
 				// 	console.log("success:", data);
 				// })
 			} else {
-				pics.push(url);
+				pics.push({img: img, url: url, lon: lon, lat: lat});
 			}
+
 		})
 
 	}
