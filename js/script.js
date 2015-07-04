b3bbc81f18395877360048138dcb9016efcca1da
Maxim Shishmarev <msgmaxim@gmail.com>
2015-07-04 19:08:38 +1000

    image gallaries

--- js/script.js
+++ js/script.js
@@ -7,17 +7,22 @@ function stories_to_pics(stories) {
 
 	var pics = [];
 
-	// stories.forEach(function(dates) {
 	for (var date in stories) {
 
-		// console.log(date);
-
 		stories[date].forEach(function(story) {
-			pics.push(story["Primary image"]);
+			var url = story["Primary image"];
+
+			if (url === "") {
+				/// got to xml and get random picture from there
+				// d3.xml("http://www.abc.net.au/local/photos/2013/08/28/3836057-mediarss.xml", function(data) {
+				// 	console.log("success:", data);
+				// })
+			} else {
+				pics.push(url);
+			}
 		})
 
 	}
-	// })
 
 	console.log(pics);
 
